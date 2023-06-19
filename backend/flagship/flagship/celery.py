76e82bc20edcd6790dd4698a1fb29f9140e08dea
pranshumalhotra <pranshu.malhotra@nagarro.com>
2023-06-19 22:37:30 +0530

    fix celery SETTINGS

--- backend/flagship/flagship/celery.py
+++ backend/flagship/flagship/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flagship.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flagship.settings.prod")
 app = Celery("flagship", include=['flagship.tasks'])
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
