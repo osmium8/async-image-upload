19fdc87826b312f4744f69ffa06b861a2ee68d79
pranshumalhotra <pranshu.malhotra@nagarro.com>
2023-06-19 22:23:48 +0530

    add celery worker command

--- backend/flagship/build.sh
+++ backend/flagship/build.sh
@@ -5,6 +5,8 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+celery --app=flagship  worker -l INFO --pool=solo
+
 # python flagship/manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
