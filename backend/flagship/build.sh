b28b4dff0459b171f2c938f952d9f026833ba06c
pranshumalhotra <pranshu.malhotra@nagarro.com>
2023-06-19 23:25:43 +0530

    remove celery command

--- backend/flagship/build.sh
+++ backend/flagship/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-celery --app=flagship  worker -l INFO --pool=solo
-
 # python flagship/manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
