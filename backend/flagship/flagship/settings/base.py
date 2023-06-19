b227a7045b32c5cee75c401e895e440bbe88fd01
pranshumalhotra <pranshu.malhotra@nagarro.com>
2023-06-19 17:50:29 +0530

    render configs

--- backend/flagship/flagship/settings/base.py
+++ backend/flagship/flagship/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import datetime
 
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-anolwn&_7$hwb07dk)utus$*n5xy=xeihpejd8p=a(=swvjsxe'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-anolwn&_7$hwb07dk)utus$*n5xy=xeihpejd8p=a(=swvjsxe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
--- backend/flagship/flagship/settings/prod.py
+++ backend/flagship/flagship/settings/prod.py
@@ -2,3 +2,28 @@ from .base import *
 
 # specify ip-address(es)
 ALLOWED_HOSTS = ['*'] 
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '<name>',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }
+}
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
+# specify ip-address(es)
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:4200',
+)
